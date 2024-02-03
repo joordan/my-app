00cbe17014954db3edfe3a32c49a50f099bb3922
Jordan <jordanespejo@gmail.com>
2024-02-03 00:29:29 +0000

    Add home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
       <div>
+        <Link href="/">Home</Link>
         <Link href="/performance">Performance</Link>
         <Link href="/reliability">Reliability</Link>
         <Link href="/scale">Scale</Link>
