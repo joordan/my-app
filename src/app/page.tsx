8f2ef2cccbdd69be5eafe57ec7a1730f2616faf2
Jordan <jordanespejo@gmail.com>
2024-02-06 05:36:22 +0000

    add styling to scale img to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,16 @@ import Image from 'next/image';
 import homeImg from '/public/home.jpg'
 export default function Home() {
   return (
-  <div>
-    Home Page
-    <Image
-      src={homeImg}
-      alt="car factory" />
-
-  </div>);
+    <div>
+      Home Page
+      <div className="absolute -z-10 inset-0">
+        <Image
+          src={homeImg}
+          alt="car factory"
+          fill
+          style={{ objectFit: 'cover' }}
+        />
+      </div>
+    </div>
+  );
 }
